adada12d0668c28560892316bb6c03149f159a94
Christian D. Glissov <chris.glissov@gmail.com>
2020-09-06 01:55:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name='recurrent-flows-msc',
+    name='deepflows',
     version='0.1.0',
     packages=find_packages(),
 )
