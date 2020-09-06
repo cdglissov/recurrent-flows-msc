772e1b0a86ff86b2adbd09530435517880d96b51
Christian D. Glissov <chris.glissov@gmail.com>
2020-09-06 23:25:55 +0200

    Update halfmoon.py

--- data_generators/halfmoon.py
+++ data_generators/halfmoon.py
@@ -7,7 +7,7 @@ def sample_half_moons(n_train, n_test, noise=0.1):
     test_data, test_labels = make_moons(n_samples=n_test, noise=noise)
     return train_data, train_labels, test_data, test_labels
 
-#Implement transform
+# TODO: Implement transform
 def loader_half_moons(train_data, test_data, batch_size):
     train_loader = Data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
     test_loader = Data.DataLoader(test_data, batch_size=batch_size, shuffle=False)
