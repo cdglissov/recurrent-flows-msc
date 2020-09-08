60b8ac65d4ed1e13ce96ac5d132e37d5ac1b0143
Christian D. Glissov <chris.glissov@gmail.com>
2020-09-08 11:57:20 +0200

    Update plot_tools.py

--- plot_tools.py
+++ plot_tools.py
@@ -24,12 +24,10 @@ def show_2d_latents(latents, labels, title='Latent Space'):
     plt.show()
 
 
-def show_2d_densities(densities, title='Densities'):
+def show_2d_densities(densities, x_lim, y_lim, title='Densities'):
     plt.figure()
     plt.title(title)
     dx, dy = 0.025, 0.025
-    x_lim = (-1.5, 2.5)
-    y_lim = (-1, 1.5)
     y, x = np.mgrid[slice(y_lim[0], y_lim[1] + dy, dy),
                     slice(x_lim[0], x_lim[1] + dx, dx)]
     plt.pcolor(x, y, densities.reshape([y.shape[0], y.shape[1]]))
@@ -38,10 +36,10 @@ def show_2d_densities(densities, title='Densities'):
     plt.ylabel('z2')
     plt.show()
 
-def show_2d_heatmap(fun, device, x_lim=(-1.5, 2.5), y_lim=(-1, 1.5), title="Densities"):
+def show_2d_heatmap(fun, device, x_lim=(-2.4, 2.4), y_lim=(-1.5, 1.5), title="Densities"):
     dx, dy = 0.025, 0.025
     y, x = np.mgrid[slice(y_lim[0], y_lim[1] + dy, dy),
                     slice(x_lim[0], x_lim[1] + dx, dx)]
     mesh_xs = torch.FloatTensor(np.stack([x, y], axis=2).reshape(-1, 2)).to(device)
     densities = np.exp(get_numpy(fun(mesh_xs)))
-    show_2d_densities(densities, title)
+    show_2d_densities(densities, x_lim, y_lim, title)
