70d136de3abbc7f1a9e668dabf52e7ba2d7b3249
Christian D. Glissov <chris.glissov@gmail.com>
2020-10-18 12:45:07 +0200

    Update glow.py

--- glow.py
+++ glow.py
@@ -11,6 +11,20 @@ import torch.nn.functional as F
 
 device = set_gpu(True)
 
+
+class ActFun(nn.Module):
+  def __init__(self, non_lin):
+    super(ActFun, self).__init__()
+    if non_lin=='relu':
+      self.net=nn.ReLU()
+    if non_lin=='leakyrelu':
+      self.net=nn.LeakyReLU(negative_slope=0.20)
+
+  def forward(self,x):
+    return self.net(x)
+
+non_lin = "leakyrelu"
+
 class ConditionalActNorm(nn.Module):
     def __init__(self, x_size, condition_size):
         """
@@ -19,23 +33,23 @@ class ConditionalActNorm(nn.Module):
         super(ConditionalActNorm, self).__init__()
         Bx, Cx, self.Hx, self.Wx = x_size
         B, C, self.H, self.W = condition_size
-
+        
         x_hidden_channels = 128
         x_hidden_size = 64
         self.ConditionalNet = nn.Sequential(
             Conv2dResize(in_size=[C, self.H, self.W], out_size=[x_hidden_channels, self.H//2, self.W//2]),
-            nn.ReLU(),
+            ActFun(non_lin),
             Conv2dResize(in_size=[x_hidden_channels, self.H//2, self.W//2], out_size=[x_hidden_channels, self.H//4, self.W//4]),
-            nn.ReLU(),
+            ActFun(non_lin),
             Conv2dResize(in_size=[x_hidden_channels, self.H//4, self.W//4], out_size=[x_hidden_channels, self.H//8, self.W//8]),
-            nn.ReLU()
+            ActFun(non_lin),
         )
         
         self.ConditionalNet_Linear = nn.Sequential(
             LinearZeros(x_hidden_channels*self.H*self.W//(8*8), x_hidden_size),
-            nn.ReLU(),
+            ActFun(non_lin),
             LinearZeros(x_hidden_size, x_hidden_size),
-            nn.ReLU(),
+            ActFun(non_lin),
             LinearZeros(x_hidden_size, 2*Cx),
             nn.Tanh()
         )
@@ -46,7 +60,7 @@ class ConditionalActNorm(nn.Module):
         output=output.view(bs, -1)
         output=self.ConditionalNet_Linear(output).view(bs, -1, 1, 1)
         
-        log_scale, shift = split_feature(output)
+        log_scale, shift = utils.split_feature(output)
 
 
         dlogdet = self.Hx * self.Wx * torch.sum(log_scale, dim=(1,2,3))
@@ -74,24 +88,24 @@ class ConditionalInvConv(nn.Module):
         x_hidden_size = 64
         self.ConditionalNet = nn.Sequential(
             Conv2dResize(in_size=[C, self.H, self.W], out_size=[x_hidden_channels, self.H//2, self.W//2]),
-            nn.ReLU(),
+            ActFun(non_lin),
             Conv2dResize(in_size=[x_hidden_channels, self.H//2, self.W//2], out_size=[x_hidden_channels, self.H//4, self.W//4]),
-            nn.ReLU(),
+            ActFun(non_lin),
             Conv2dResize(in_size=[x_hidden_channels, self.H//4, self.W//4], out_size=[x_hidden_channels, self.H//8, self.W//8]),
-            nn.ReLU()
+            ActFun(non_lin),
         )
 
         self.ConditionalNet_Linear = nn.Sequential(
             LinearZeros(x_hidden_channels*self.H*self.W//(8*8), x_hidden_size),
-            nn.ReLU(),
+            ActFun(non_lin),
             LinearZeros(x_hidden_size, x_hidden_size),
-            nn.ReLU(),
+            ActFun(non_lin),
             LinearNorm(x_hidden_size, Cx*Cx),
             nn.Tanh()
         )
 
     def forward(self, x, condition, logdet = None, reverse=False):
-
+        ### WE NOTICE PROBLEMS HERE. THERE IS NOISE DUE TO W NOT BEING AN ORTHOGONAL MATRIX. 
         Bx, Cx, Hx, Wx = x.size()
         B, C, H, W = condition.size()
 
@@ -99,25 +113,21 @@ class ConditionalInvConv(nn.Module):
         bs = x.shape[0]
         W = self.ConditionalNet(condition)
         W=W.view(bs, -1)
-        W=self.ConditionalNet_Linear(W)
+        W=self.ConditionalNet_Linear(W).view(bs, Cx, Cx, 1)
 
         W = W.view(Bx, Cx, Cx)
         
-        # Bug in pytorch for QR with small matrices using cuda. Move to CPU
-        W = torch.qr(W.cpu())[0]
-        W=W.cuda()
 
-        dlogdet = Hx*Wx*torch.slogdet(W.double())[1].float()
+        dlogdet = Hx*Wx*torch.slogdet(W)[1]
 
         if reverse == False:
-            W = W.view(Bx, Cx, Cx, 1, 1).contiguous()
+            W = W.view(Bx, Cx, Cx, 1, 1)
         else:
-            W = torch.inverse(W.double()).float().view(Bx, Cx, Cx, 1, 1).contiguous()
+            W = torch.inverse(W.double()).float().view(Bx, Cx, Cx, 1, 1)
         
         x = x.view(1, Bx*Cx, Hx, Wx)
         B_weight, C_1_weight, C_2_weight, H_weight, W_weight = W.shape
-
-        W = W.view(B_weight*C_1_weight, C_2_weight, H_weight, W_weight)
+        W = W.reshape(B_weight*C_1_weight, C_2_weight, H_weight, W_weight)
 
         if reverse == False:
           x = F.conv2d(x, W, groups=Bx)
@@ -143,31 +153,31 @@ class ConditionalAffineCoupling(nn.Module):
 
         self.resize = nn.Sequential(
             Conv2dZeros(C, 16),
-            nn.ReLU(),
+            ActFun(non_lin),
             Conv2dResize((16, H, W), out_size=(Cx, Hx, Wx)),
-            nn.ReLU(),
+            ActFun(non_lin),
             Conv2dZeros(Cx, Cx//2),
-            nn.ReLU()
+            ActFun(non_lin),
         )
 
         self.f = nn.Sequential(
             Conv2dNormy(Cx, hidden_channels),
-            nn.ReLU(),
+            ActFun(non_lin),
             Conv2dNormy(hidden_channels, hidden_channels, kernel_size=[1, 1]),
-            nn.ReLU(),
+            ActFun(non_lin),
             Conv2dZerosy(hidden_channels, Cx),
             nn.Tanh()
         )
 
     def forward(self, x, condition, logdet=0.0, reverse=False):
         
-        z1, z2 = split_feature(x, "split")
+        z1, z2 = utils.split_feature(x, "split")
         condition = self.resize(condition)
         x = torch.cat((z1, condition), dim=1)
         
         x = self.f(x)
 
-        scale, shift = split_feature(x, "cross")
+        scale, shift = utils.split_feature(x, "cross")
         scale = torch.sigmoid(scale + 2.)
 
         if reverse == False:
@@ -182,50 +192,6 @@ class ConditionalAffineCoupling(nn.Module):
         x = torch.cat((z1, z2), dim=1)
         return x, logdet
 
-
-class Squeeze2d(nn.Module):
-    def __init__(self):
-        super(Squeeze2d, self).__init__()
-        
-    def forward(self, x, undo_squeeze = False):
-      B, C, H, W = x.shape
-      if undo_squeeze == False:
-        # C x H x W -> 4C x H/2 x W/2
-        x = x.view(B, C, H // 2, 2, W // 2, 2)
-        x = x.permute(0, 1, 3, 5, 2, 4).contiguous()
-        x = x.view(B, C * 4, H // 2, W // 2)
-      else:
-        # 4C x H/2 x W/2  ->  C x H x W
-        x = x.view(B, C // 4, 2, 2, H, W)
-        x = x.permute(0, 1, 4, 2, 5, 3).contiguous()
-        x = x.view(B, C // 4, H * 2, W * 2)
-      return x
-
-class Split2d(nn.Module):
-    def __init__(self, x_size):
-      super(Split2d, self).__init__()
-
-      Bx, Cx, Hx, Wx = x_size
-      self.conv = nn.Sequential(
-          Conv2dZeros(Cx // 2, Cx),
-          #nn.Tanh()
-          )
-      
-
-    def forward(self, x, logdet=0.0, reverse=False):
-        
-        
-        if reverse == False:
-            z1, z2 = split_feature(x, "split")
-            out = self.conv(z1)
-            mean, log_scale = split_feature(out, "cross")
-            logdet += torch.sum(td.Normal(mean, torch.exp(log_scale)).log_prob(z2), dim=(1,2,3))
-            return z1, logdet
-        else:
-            mean, log_scale = split_feature(self.conv(x), "cross")
-            z2 = td.Normal(mean, torch.exp(log_scale)).sample()#.rsample()
-            z = torch.cat((x, z2), dim=1)
-            return z, logdet
 
 class GlowStep(nn.Module):
     def __init__(self, x_size, condition_size):
@@ -247,6 +213,50 @@ class GlowStep(nn.Module):
             x, logdet = self.invconv(x, condition, logdet, reverse=True)
             x, logdet = self.actnorm(x, condition, logdet, reverse=True)
             return x, logdet
+
+class Squeeze2d(nn.Module):
+    def __init__(self):
+        super(Squeeze2d, self).__init__()
+        
+    def forward(self, x, undo_squeeze = False):
+      B, C, H, W = x.shape
+      if undo_squeeze == False:
+        # C x H x W -> 4C x H/2 x W/2
+        x = x.reshape(B, C, H // 2, 2, W // 2, 2)
+        x = x.permute(0, 1, 3, 5, 2, 4)
+        x = x.reshape(B, C * 4, H // 2, W // 2)
+      else:
+        # 4C x H/2 x W/2  ->  C x H x W
+        x = x.reshape(B, C // 4, 2, 2, H, W)
+        x = x.permute(0, 1, 4, 2, 5, 3)
+        x = x.reshape(B, C // 4, H * 2, W * 2)
+      return x
+
+class Split2d(nn.Module):
+    def __init__(self, x_size):
+      super(Split2d, self).__init__()
+
+      Bx, Cx, Hx, Wx = x_size
+      self.conv = nn.Sequential(
+          Conv2dZeros(Cx // 2, Cx),
+          ActFun(non_lin),  # Some include tanh or none here, we found leakyrelu to work better
+          )
+      
+
+    def forward(self, x, logdet=0.0, reverse=False):
+        
+        
+        if reverse == False:
+            z1, z2 = utils.split_feature(x, "split")
+            out = self.conv(z1)
+            mean, log_scale = utils.split_feature(out, "cross")
+            logdet += torch.sum(td.Normal(mean, torch.exp(log_scale)).log_prob(z2), dim=(1,2,3))
+            return z1, logdet
+        else:
+            mean, log_scale = utils.split_feature(self.conv(x), "cross")
+            z2 = td.Normal(mean, torch.exp(log_scale)).rsample()
+            z = torch.cat((x, z2), dim=1)
+            return z, logdet
 
 class GlowConditional(nn.Module):
     def __init__(self, x_size, condition_size, K = 4, L = 2, learn_prior = True):
@@ -275,12 +285,20 @@ class GlowConditional(nn.Module):
 
         self.glow_frame = nn.ModuleList(layers)
 
+        self.learn_prior = learn_prior
+        if learn_prior:
+          self.new_mean = nn.Parameter(torch.zeros([Cx, Hx, Wx])).to(device)
+          self.new_logs = nn.Parameter(torch.zeros([Cx, Hx, Wx])).to(device)
+        else:
+          self.new_mean = torch.zeros([Cx, Hx, Wx]).to(device)
+          self.new_logs = torch.zeros([Cx, Hx, Wx]).to(device)
 
-    def set_prior(self, mean, scale):
-        self.prior = td.Normal(mean, scale)
+    def set_prior(self):
+        return self.new_mean, torch.exp(self.new_logs)
 
-    def g(self, x, condition, logdet=0.0):
+    def g(self, z, condition, logdet=0.0):
         # maps z -> x
+        x=z
         for step in reversed(self.glow_frame):
           if isinstance(step, Squeeze2d):
             x = step(x, undo_squeeze=True)
@@ -290,8 +308,9 @@ class GlowConditional(nn.Module):
             x, _ = step(x, condition=condition, logdet = logdet, reverse=True)
         return x, logdet
 
-    def f(self, z, condition, logdet=0.0):
+    def f(self, x, condition, logdet=0.0):
         # maps x -> z
+        z = x
         for step in self.glow_frame:
             if isinstance(step, Squeeze2d):
                 z = step(z)
@@ -302,23 +321,27 @@ class GlowConditional(nn.Module):
         return z, logdet
     
     def log_prob(self, x, condition):
-        #x, nll_discretization = self.uniform_binning_correction(x)
+        x, nll_discretization = self.uniform_binning_correction(x)
         #condition, _ = self.uniform_binning_correction(condition)
         dims = torch.prod(torch.tensor(x.shape[1:]))
         z, obj = self.f(x, condition, 0.0)
-        obj += torch.sum(self.prior.log_prob(z), [1, 2, 3]) #p_z
+        mean, scale = self.set_prior()
+        prior = td.Normal(mean, scale)
+        obj += torch.sum(prior.log_prob(z), [1, 2, 3]) #p_z
         obj = torch.mean(obj)
         nll = -(obj) / float(np.log(2.) * dims) #+ nll_discretization.mean(0)
         return z, nll
 
-    def sample(self, z, condition):
+    def sample(self, z, condition, num_samples):
         with torch.no_grad():
+          mean, scale = self.set_prior()
+          prior = td.Normal(mean, scale)
           if z == None:
-            z = self.prior.sample().to(device)
+            z = prior.sample([num_samples]).to(device)
           x, logdet = self.g(z, condition)
         return x, logdet
 
-    def uniform_binning_correction(self, x, n_bits=32):
+    def uniform_binning_correction(self, x, n_bits=8):
       n_bins = 2**n_bits
       batch_size, n_channels, height, width = x.shape
       hwc = float(height * width * n_channels)
