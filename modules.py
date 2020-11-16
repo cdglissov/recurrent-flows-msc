1b9d24c53b40b03c4150973df7ee6b3d53c06484
TobiasKonradsen <43344305+TobiasKonradsen@users.noreply.github.com>
2020-11-16 16:42:28 +0100

    Create modules.py

---
+++ modules.py
@@ -0,0 +1,138 @@
+import torch
+import torch.nn as nn
+from .utils import *
+
+
+class ActFun(nn.Module):
+  def __init__(self, non_lin, in_place = False):
+    super(ActFun, self).__init__()
+    if non_lin=='relu':
+      self.net=nn.ReLU(inplace = in_place)
+    elif non_lin=='leakyrelu':
+      self.net=nn.LeakyReLU(negative_slope=0.20, inplace = in_place)
+    else:
+      assert False, 'Please specify a activation type from the set {relu,leakyrelu}'
+ 
+  def forward(self,x):
+    return self.net(x)
+
+class NoNorm(nn.Module):
+    def __init__(self):
+      super(NoNorm, self).__init__()
+    
+    def forward(self, x):
+      return x
+
+class NormLayer(nn.Module):
+    def __init__(self, in_channels, norm_type):
+      super(NormLayer, self).__init__()
+      if norm_type =='batchnorm':
+        self.norm = nn.BatchNorm2d(in_channels)
+      elif norm_type =='instancenorm':
+        self.norm = nn.InstanceNorm2d(in_channels)
+      elif norm_type =='none':
+        self.norm = NoNorm()
+      else:
+        assert False, 'Please specify a norm type from the set {batchnorm, instancenorm, none}'
+    
+    def forward(self, x):
+      return self.norm(x)
+
+class VGG_downscaler(nn.Module):
+  def __init__(self, structure, in_channels = 1, norm_type = "batchnorm", non_lin = "leakyrelu", scale=2):
+    super(VGG_downscaler, self).__init__()
+
+    layers = []
+    for i in structure:
+        if i == 'pool':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        elif i == "conv":
+            conv_channels = int(in_channels*scale)
+            conv2d = nn.Conv2d(in_channels, conv_channels, kernel_size=3, stride = 2, padding=1)
+            layers += [conv2d,
+                       NormLayer(conv_channels, norm_type = norm_type), 
+                       ActFun(non_lin, in_place=True)]
+            in_channels = conv_channels
+        else:
+            conv2d = nn.Conv2d(in_channels, i, kernel_size=3, padding=1)
+            layers += [conv2d, NormLayer(i, norm_type = norm_type), ActFun(non_lin, in_place=True)]
+            in_channels = i
+    
+    self.net = nn.Sequential(*layers)
+
+  def forward(self, x):
+    return self.net(x)
+
+
+class VGG_upscaler(nn.Module):
+  def __init__(self, structures, L, in_channels, norm_type = "batchnorm", non_lin = "relu", scale = 2):
+    super(VGG_upscaler, self).__init__()
+    assert len(structures) == L, "Please specify number of blocks = L"
+    self.l_nets = []
+    self.L = L
+
+    for l in range(0, L):
+      structure = structures[l]
+      layers = []
+      for i in structure:
+          if i == 'upsample':
+              layers += [nn.Upsample(scale_factor=2, mode='nearest')]
+          elif i == "deconv":
+              deconv_channels = in_channels // scale
+              deconv = nn.ConvTranspose2d(in_channels, deconv_channels, kernel_size=4, stride = 2, padding=1, bias=False)
+              layers += [deconv, 
+                        NormLayer(deconv_channels, norm_type = norm_type),
+                        ActFun(non_lin, in_place=True)]
+              in_channels = deconv_channels
+          else:
+              conv2d = nn.Conv2d(in_channels, i, kernel_size=3, stride=1, padding=1)
+              layers += [conv2d,  NormLayer(i, norm_type = norm_type), ActFun(non_lin, in_place=True)]
+              in_channels = i
+      
+          self.net = nn.Sequential(*layers).to(device)
+          # for m in self.modules():
+          #   if isinstance(m, nn.Conv2d):
+          #       m.weight.data.normal_(0, 0.05)
+          #       m.bias.data.zero_()
+          #   elif isinstance(m, nn.ConvTranspose2d):
+          #       m.weight.data.normal_(0, 0.05)
+
+      self.l_nets.append(self.net)
+
+  def forward(self, x, block_size=None):
+    outputs = []
+    for i in range(0, self.L):
+      x = self.l_nets[i](x)
+      outputs.append(x)
+    outputs.reverse()
+    return outputs
+
+class SimpleParamNet(nn.Module):
+  def __init__(self, structure, in_channels, out_channels, norm_type = "batchnorm", non_lin = "leakyrelu", scale = 2):
+    super(SimpleParamNet, self).__init__()
+
+    layers = []
+    for i in structure:
+        if i == 'pool':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        elif i == "conv":
+            conv_channels = int(scale*in_channels)
+            conv2d = nn.Conv2d(in_channels, conv_channels, kernel_size=3, stride = 2, padding=1)
+            layers += [conv2d,
+                       NormLayer(conv_channels, norm_type = norm_type), 
+                       ActFun(non_lin, in_place=True)]
+            in_channels = conv_channels
+        else:
+            conv2d = nn.Conv2d(in_channels, i, kernel_size=3, padding=1)
+            layers += [conv2d, NormLayer(i, norm_type = norm_type), ActFun(non_lin, in_place=True)]
+            in_channels = i
+    
+    self.net = nn.Sequential(*layers)
+    self.loc = nn.Conv2d(in_channels, out_channels,  kernel_size=3, stride=1, padding=1)
+    self.scale = nn.Sequential(nn.Conv2d(in_channels, out_channels,  kernel_size=3, stride=1, padding=1), nn.Softplus())
+    
+  def forward(self, x):
+    output = self.net(x)
+    loc = self.loc(output)
+    scale = self.scale(output)
+    return loc, scale
