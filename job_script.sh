af61c0217eeaca2fca7748f9a7caea075c520cbc
TobiasKonradsen <43344305+TobiasKonradsen@users.noreply.github.com>
2020-11-19 14:00:17 +0100

    Add files via upload

--- job_script.sh
+++ job_script.sh
@@ -1,7 +1,7 @@
 # !/bin/sh
-source "/work1/s144077/train_env/bin/activate"
-###  Name of jobscript 
-#BSUB -J testv8
+##### source "/work1/s144077/train_env/bin/activate"
+###  Name of jobscript
+#BSUB -J testv12
 ### -- GPU --
 #BSUB -q gpuv100
 ### -- How many gpus and hvor to run --
@@ -11,16 +11,16 @@ source "/work1/s144077/train_env/bin/activate"
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
 #BSUB -W 00:30
 ### -- How many nodes the job requests -- ##
-#BSUB -R "span[hosts=1]" 
+#BSUB -R "span[hosts=1]"
 ### -- How much ram the requests -- ##
-#BSUB -R "rusage[mem=1GB]" 
+#BSUB -R "rusage[mem=32GB]"
 ### -- How many nodes the job requests -- ##
 # For requesting the extra big GPU w. 32GB of VRAM
-#BSUB -R "select[gpu32gb]" 
+###BSUB -R "select[gpu32gb]"
 
 #BSUB -N Send an email when done
-#BSUB -o testv8/%J_log_fil.out Log file
-#BSUB -e testv8/%J_Error_file.err Error log file
+#BSUB -o testv11/%J_log_fil.out Log file
+#BSUB -e testv11/%J_Error_file.err Error log file
 
 echo "Starting:"
 
@@ -31,12 +31,13 @@ echo "Starting:"
 cd /work1/s144077/
 . train_env/bin/activate train_env
 
-CONTENTPATH=/testv8/
+CONTENTPATH=/testv12/
 BATCH_SIZE=128
-IMAGE_SIZE=64
+IMAGE_SIZE=32
+CHANNELS=1
 echo $CONTENTPATH
 
 nvidia-smi
 
 #I run my python script with all of the settings
-python3 deepflows/main.py --path $CONTENTPATH --image_size $IMAGE_SIZE --digit_size 28 --num_digits 1 --K 3 --L 2 --extractor_structure 32 conv 32 conv 64 --upscaler_structure 64 deconv-32 --x_dim $BATCH_SIZE 1 $IMAGE_SIZE $IMAGE_SIZE --condition_dim $BATCH_SIZE 1 $IMAGE_SIZE $IMAGE_SIZE
+python3 deepflows/main.py --path $CONTENTPATH --image_size $IMAGE_SIZE --digit_size 16 --num_digits 1 --K 3 --L 2 --extractor_structure 32 conv 32 conv 64 --upscaler_structure 64 deconv-32 --x_dim $BATCH_SIZE $CHANNELS $IMAGE_SIZE $IMAGE_SIZE --condition_dim $BATCH_SIZE $CHANNELS $IMAGE_SIZE $IMAGE_SIZE
