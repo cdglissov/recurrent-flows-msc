13fe3f43c915dedb7972f5c6417f0cacd6a73ec9
TobiasKonradsen <43344305+TobiasKonradsen@users.noreply.github.com>
2020-12-07 12:35:25 +0100

    Update main.py

--- RFN/main.py
+++ RFN/main.py
@@ -6,10 +6,8 @@ from trainer import Solver
 def main(args):
     solver = Solver(args)
     solver.build()
-    #TODO: Check if this loader works
     if args.load_model:
-        path_model = args.path + 'model_folder/rfn.pt'
-        solver.load(path_model)
+        solver.load()
     solver.train()   
 
     
@@ -54,13 +52,13 @@ if __name__ == "__main__":
     parser.add_argument("--choose_data", help="Specify dataset", 
                         choices=['mnist', 'bair'], default='mnist', type=str)
     parser.add_argument("--image_size", help="Specify the image size of mnist", 
-                        default=32, type=int)
+                        default=64, type=int)
     parser.add_argument("--digit_size", help="Specify the size of mnist digit", 
-                        default=21, type=int)
+                        default=28, type=int)
     parser.add_argument("--step_length", help="Specify the step size of mnist digit", 
                         default=4, type=int)
     parser.add_argument("--num_digits", help="Specify the number of mnist digits", 
-                        default=1, type=int)
+                        default=2, type=int)
     parser.add_argument("--num_workers", help="Specify the number of workers in dataloaders", 
                         default=0, type=int)
     
@@ -78,7 +76,7 @@ if __name__ == "__main__":
     parser.add_argument("--n_bits", help="Specify number of bits", 
                         default=8, type=int)
     parser.add_argument("--n_epochs", help="Specify number of epochs", 
-                        default=100, type=int)
+                        default=100000, type=int)
     add_bool_arg(parser, "verbose", default=False, help="Specify verbose mode (boolean)")
     parser.add_argument("--path", help="Specify path to experiment", 
                         default='/content/', type=str)
@@ -89,62 +87,67 @@ if __name__ == "__main__":
     parser.add_argument("--preprocess_scale", help="Specify the scale for preprocessing", 
                         default=255, type=int)
     parser.add_argument("--beta_max", help="Specify the maximum value of beta", 
-                        default=0.01, type=float)
+                        default=1, type=float)
     parser.add_argument("--beta_min", help="Specify the minimum value of beta", 
                         default=0.0001, type=float)
     parser.add_argument("--beta_steps", help="Specify the annealing steps", 
-                        default=2000, type=int)
+                        default=50000, type=int)
     parser.add_argument("--n_predictions", help="Specify number of predictions", 
                         default=6, type=int)
     add_bool_arg(parser, "multigpu", default=False, help="Specify if we want to use multi GPUs")
     add_bool_arg(parser, "load_model", default=False, 
                  help="Specify if we want to load a pre-existing model (boolean)")
-    add_bool_arg(parser, "variable_beta", default=True, 
-                 help="Specify if we want a linear decreasing beta value (boolean)")
-    
-    
     # RFN
     parser.add_argument('--x_dim', nargs='+', help="Specify data dimensions (b,c,h,w)", 
-                        default=[64, 1, 32, 32], type=int)
+                        default=[128, 1, 64, 64], type=int)
     parser.add_argument('--condition_dim', nargs='+', help="Specify condition dimensions (b,c,h,w)", 
-                        default=[64, 1, 32, 32], type=int)
+                        default=[128, 1, 64, 64], type=int)
     parser.add_argument("--h_dim", help="Specify hidden state (h) channels", 
                         default=100, type=int)
     parser.add_argument("--z_dim", help="Specify latent (z) channels", 
                         default=30, type=int)
     parser.add_argument("--L", help="Specify flow depth", 
-                        default=4, type=int)
+                        default=3, type=int)
     parser.add_argument("--K", help="Specify flow recursion", 
                         default=10, type=int)
-    parser.add_argument("--c_features", help="Specify channels of extracted features", 
-                        default=128, type=int)
-    parser.add_argument('--extractor_structure', nargs='+', help="Specify structure of extractor (int, str)", 
-                        default= [32, 'conv', 32, 'conv', 64, 'conv', 64, 'conv'], type=convert_mixed_list)
+    # Downscaler architechture. Consisting of L blocks, the end of each block
+    # will be what is skip connected. It is possible to chose between 
+    # pool,conv,squeeze, as downsampling methods. Does not need to end with integer
+    parser.add_argument('--extractor_structure', nargs='+', help="Specify structure of extractor example writing, 32-32-conv 32-32-pool, creates 2 blocks", 
+                        default= [[1, 2, 'squeeze'],[8, 8, 'squeeze'], [32, 32, 'squeeze']], type=convert_to_upscaler)
     parser.add_argument('--norm_type', help="Specify normalization type of layers", 
                         default='none', choices=["instancenorm", "batchnorm", "none"], type=str)
+    add_bool_arg(parser, "skip_connection", default=True, help="Specify skip_connections mode (boolean)")
     # Upscaler structure can be a bit tricky to define. First the input does not need to be fully upscaled,
     # so for L = 3 only 2 deconv's is required. Every block should end with an integer. I.e. 32-deconv-32 deconv-16
     # and not 32-deconv 32-deconv
     parser.add_argument('--upscaler_structure', help="Specify upscaler structure, example writing, 32-32-deconv 32-32-upsample, creates 2 blocks",
-                        nargs='+', default=[[128], ['deconv', 64], ['deconv', 64], ['deconv', 32]], type=convert_to_upscaler)
+                        nargs='+', default=[[64], ['squeeze', 32, 32], ['squeeze', 16, 16]], type=convert_to_upscaler)
     parser.add_argument("--structure_scaler", help="Specify down/up-sampling channel factor", 
-                        default=1, type=int)
+                        default=2, type=int)
     parser.add_argument("--temperature", help="Specify temperature", 
                         default=0.8, type=restricted_float)
     parser.add_argument("--prior_structure", help="Specify the structure of the prior", 
-                        nargs="+" ,default=[128],type=convert_mixed_list)
+                        nargs="+" ,default=[256, 128],type=convert_mixed_list)
     parser.add_argument("--encoder_structure", help="Specify the structure of the encoder", 
                         nargs="+" ,default=[256, 128],type=convert_mixed_list)
+    parser.add_argument('--norm_type_coders', help="Specify normalization type of layers upscaler/downscaler", 
+                        default='batchnorm', choices=["instancenorm", "batchnorm", "none"], type=str)
     #Glow
     add_bool_arg(parser, "learn_prior", default=True, help="Specify if we want a learned prior (boolean)")
     add_bool_arg(parser, "LU_decomposed", default=True, help="Specify if we want to use LU factorization (boolean)")
     parser.add_argument("--n_units_affine", help="Specify hidden units in affine coupling", 
                         default=128, type=int)
     parser.add_argument("--n_units_prior", help="Specify hidden units in prior", 
-                        default=256, type=int)
+                        default=512, type=int)
     add_bool_arg(parser, "make_conditional", default=True, 
                  help="Specify if split should be conditional or not (boolean)")
-    
+    parser.add_argument('--flow_norm', help="Specify normalization type of glow-step", 
+                        default='actnorm', choices=["batchnorm", "actnorm"], type=str)
+    parser.add_argument('--base_norm', help="Specify normalization type of base distribution", 
+                        default='actnorm', choices=["batchnorm", "actnorm"], type=str)
+    parser.add_argument("--flow_batchnorm_momentum", help="Running average batchnorm momentum for flow-step", 
+                        default=0.0, type=float)
     args = parser.parse_args()
     
     main(args)
