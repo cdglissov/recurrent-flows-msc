b72ebf8c88eb822dde39b1caa0e67735cd6d0b31
Christian D. Glissov <chris.glissov@gmail.com>
2021-01-22 14:48:55 +0100

    fixed indentations

--- RFN/trainer.py
+++ RFN/trainer.py
@@ -9,7 +9,7 @@ from data_generators import PushDataset
 import matplotlib.pyplot as plt
 from .RFN import RFN
 from Utils import set_gpu
-from tqdm import tqdm 
+from tqdm import tqdm
 import math
 device = set_gpu(True)
 
@@ -29,7 +29,7 @@ class EarlyStopping():
 		current_loss = loss
 		if current_loss is None:
 			pass
-		else: 
+		else:
 			if (current_loss - self.best_loss) < -self.min_delta:
 				self.best_loss = current_loss
 				self.wait = 1
@@ -40,7 +40,7 @@ class EarlyStopping():
 					  print("STOP! Criterion met at epoch " % (self.epoch))
 					return self.stop_training
 				self.wait += 1
- 
+
 class Solver(object):
     def __init__(self, args):
         self.args = args
@@ -77,52 +77,52 @@ class Solver(object):
         self.n_predictions = args.n_predictions
         self.n_conditions = args.n_conditions
         self.scheduler_type = args.scheduler_type
-        
+
     def build(self):
         self.train_loader, self.test_loader = self.create_loaders()
-        
+
         if not os.path.exists(self.path + 'png_folder'):
           os.makedirs(self.path + 'png_folder')
         if not os.path.exists(self.path + 'model_folder'):
           os.makedirs(self.path + 'model_folder')
-        
+
         if self.multigpu and torch.cuda.device_count() > 1:
             print("Using:", torch.cuda.device_count(), "GPUs")
             self.model = nn.DataParallel(RFN(self.params)).to(device)
         else:
             self.model = RFN(self.args).to(device)
-        
-            
+
+
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
-        self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, 'min', 
-                                                                    patience=self.patience_lr, 
-                                                                    factor=self.factor_lr, 
+        self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, 'min',
+                                                                    patience=self.patience_lr,
+                                                                    factor=self.factor_lr,
                                                                     min_lr=self.min_lr,
                                                                     verbose = True)
-        self.earlystopping = EarlyStopping(min_delta = 0, patience = self.patience_es, 
+        self.earlystopping = EarlyStopping(min_delta = 0, patience = self.patience_es,
                                            verbose = self.verbose)
         self.counter = 0
         self.stop = False
-    
+
     def create_loaders(self):
         if self.choose_data=='mnist':
-            	testset = MovingMNIST(False, 'Mnist', 
-                                                         seq_len=self.n_frames, 
-                                                         image_size=self.image_size, 
-                                                         digit_size=self.digit_size, 
-                                                         num_digits=self.num_digits, 
-            												 deterministic=False, 
-                                                         three_channels=False, 
-                                                         step_length=self.step_length, 
+            	testset = MovingMNIST(False, 'Mnist',
+                                                         seq_len=self.n_frames,
+                                                         image_size=self.image_size,
+                                                         digit_size=self.digit_size,
+                                                         num_digits=self.num_digits,
+            												 deterministic=False,
+                                                         three_channels=False,
+                                                         step_length=self.step_length,
                                                          normalize=False)
-            	trainset = MovingMNIST(True, 'Mnist', 
-                                                          seq_len=self.n_frames, 
-                                                          image_size=self.image_size, 
-                                                          digit_size=self.digit_size, 
-                                                          num_digits=self.num_digits, 
-            												  deterministic=False, 
-                                                          three_channels=False, 
-                                                          step_length=self.step_length, 
+            	trainset = MovingMNIST(True, 'Mnist',
+                                                          seq_len=self.n_frames,
+                                                          image_size=self.image_size,
+                                                          digit_size=self.digit_size,
+                                                          num_digits=self.num_digits,
+            												  deterministic=False,
+                                                          three_channels=False,
+                                                          step_length=self.step_length,
                                                           normalize=False)
         if self.choose_data=='bair':
             	string=str(os.path.abspath(os.getcwd()))
@@ -136,8 +136,8 @@ class Solver(object):
         train_loader = DataLoader(trainset,batch_size=self.batch_size,num_workers=self.num_workers, shuffle=True, drop_last=True)
         test_loader = DataLoader(testset, batch_size=self.batch_size,num_workers=self.num_workers, shuffle=True, drop_last=True)
         return train_loader, test_loader
- 
-        
+
+
     def preprocess(self, x, reverse = False):
         # Remember to change the scale parameter to make variable between 0..255
         n_bits = self.n_bits
@@ -157,7 +157,7 @@ class Solver(object):
             x = x * self.preprocess_scale
             if n_bits < 8:
               x = torch.floor( x/2 ** (8 - n_bits))
-            x = x / n_bins  
+            x = x / n_bins
           else:
             x = x * n_bins
             x = torch.clamp(x * (255 / n_bins), 0, 255).byte()
@@ -175,17 +175,17 @@ class Solver(object):
             for param_group in self.optimizer.param_groups:
                 param_group['lr'] = lr
 
-            
+
         if batch == (startbatch + num_steps): # If this is true the learning rate is zero.
             self.stop = True
 
     def compute_loss(self, nll, kl_free_bit, kl, dims, t=10):
         loss = nll + self.beta*kl_free_bit
-        
+
         kl_store = kl.data
         nll_store = nll.data
         elbo = -(kl_store+nll_store)
-        
+
         #https://stats.stackexchange.com/questions/423120/what-is-bits-per-dimension-bits-dim-exactly-in-pixel-cnn-papers/431012
         bits_per_dim_loss = -elbo/(np.log(2.)*torch.prod(torch.tensor(dims))*t)
         self.bits.append(float(bits_per_dim_loss))
@@ -193,13 +193,13 @@ class Solver(object):
         self.kl_loss.append(float(kl_store)/t)
         self.recon_loss.append(float(nll_store)/t)
         return loss
-    
+
     def train(self):
-      
+
       max_value = self.beta_max
       min_value = self.beta_min
       num_steps = self.beta_steps
-      
+
       for epoch_i in range(self.n_epochs):
           self.model.train()
           self.epoch_i += 1
@@ -211,45 +211,45 @@ class Solver(object):
             else:
                 image = image.to(device)
             image = self.preprocess(image)
-	    logdet = 0
+	    	logdet = 0
             self.beta = min(max_value, min_value + self.counter*(max_value - min_value) / num_steps)
-            
+
             if self.multigpu and torch.cuda.device_count() > 1:
                 kl_free_bit, kl, nll = self.model.module.loss(image, logdet)
             else:
                 kl_free_bit, kl, nll = self.model.loss(image, logdet)
-            loss = self.compute_loss(nll, kl_free_bit,  kl, image.shape[2:], t=image.shape[1]-1) # Minus one as we condition on frame. 
+            loss = self.compute_loss(nll, kl_free_bit,  kl, image.shape[2:], t=image.shape[1]-1) # Minus one as we condition on frame.
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
             if self.scheduler_type == 'linear':
                 self.adjust_learning_rate(self.counter)
-                
+
             self.counter += 1
             #if (batch_i % 5)==0:
-          
-          self.plotter()   
+
+          self.plotter()
           epoch_loss = np.mean(self.losses)
- 
+
           if self.epoch_i % 1 == 0:
             # Save model after each epoch
-            self.checkpoint('rfn.pt', self.epoch_i, epoch_loss) 
- 
+            self.checkpoint('rfn.pt', self.epoch_i, epoch_loss)
+
           # Do early stopping, if above patience stop training, if better loss, save model
           stop = self.earlystopping.step(self.epoch_i, epoch_loss)
           if stop or self.stop:
             break
           if (self.earlystopping.best_loss < self.best_loss) and (self.epoch_i > 50):
             self.best_loss = self.earlystopping.best_loss
-            self.checkpoint('rfn_best_model.pt', self.epoch_i, epoch_loss) 
-        
+            self.checkpoint('rfn_best_model.pt', self.epoch_i, epoch_loss)
+
           if self.scheduler_type == 'plateau':
               self.scheduler.step(epoch_loss)
           if self.verbose:
             print('Epoch {} Loss: {:.2f}'.format(self.epoch_i, epoch_loss))
           else:
             self.status()
- 
+
     def checkpoint(self, model_name, epoch, loss):
       torch.save({
           'epoch': epoch,
@@ -272,7 +272,7 @@ class Solver(object):
           'annealing_counter': self.counter,
           'args': self.args,
           }, self.path + 'model_folder/eval_dict.pt')
-      
+
     def load(self, load_model):
       self.model.load_state_dict(load_model['model_state_dict'])
       self.optimizer.load_state_dict(load_model['optimizer_state_dict'])
@@ -286,7 +286,7 @@ class Solver(object):
       self.best_loss = loss
       self.model.to(device)
       return (self.epoch_i, loss)
- 
+
     def status(self):
       # Only works for python 3.x
       lr = self.optimizer.param_groups[0]['lr']
@@ -294,7 +294,7 @@ class Solver(object):
         print("STATUS:", file=f)
         print("\tKL and Reconstruction loss: {:.4f}, {:.4f}".format(self.kl_loss[-1], self.recon_loss[-1]), file=f)
         print(f'\tEpoch {self.epoch_i}, Beta value {self.beta:.4f}, Learning rate {lr}', file=f)
- 
+
     def plotter(self):
       n_plot = str(self.plot_counter)
       with torch.no_grad():
@@ -304,88 +304,88 @@ class Solver(object):
             image = image[0].to(device)
         else:
             image = image.to(device)
-        time_steps = self.n_frames 
-        
+        time_steps = self.n_frames
+
         image  = self.preprocess(image, reverse=False)
-        
+
         x_conditions, predictions = self.model.predict(image,
                                                  n_predictions = self.n_predictions,
                                                  n_conditions = self.n_conditions)
         plot_preds = torch.cat((x_conditions, predictions), 0)
-        
+
         recons, recons_flow = self.model.reconstruct(image)
         samples = self.model.sample(image, n_samples = time_steps)
-        
+
         samples  = self.preprocess(samples, reverse=True)
         recons  = self.preprocess(recons, reverse=True)
         recons_flow  = self.preprocess(recons_flow, reverse=True)
         plot_preds  = self.preprocess(plot_preds, reverse=True)
         image  = self.preprocess(image, reverse=True)
-    
- 
+
+
       fig, ax = plt.subplots(1, 4 , figsize = (20,5))
       ax[0].plot(self.bits)
       ax[0].set_title("Bits per dim")
       ax[0].set_xlabel("mini-batch")
       ax[0].set_ylabel("bits-per-dim")
       ax[0].grid()
-      
+
       ax[1].plot(self.losses)
       ax[1].set_title("Log of the loss")
       ax[1].set_yscale('log')
       ax[1].set_xlabel("mini-batch")
       ax[1].set_ylabel("log-loss")
       ax[1].grid()
-      
+
       ax[2].plot(self.kl_loss)
       ax[2].set_title("KL Loss")
       ax[2].grid()
       ax[2].set_xlabel("mini-batch")
       ax[2].set_ylabel("KL")
-      
+
       ax[3].plot(self.recon_loss)
       ax[3].set_title("Reconstruction Loss")
       ax[3].grid()
       ax[3].set_xlabel("mini-batch")
       ax[3].set_ylabel("nll")
-      
+
       if not self.verbose:
-	fig.tight_layout()
+		fig.tight_layout()
         fig.savefig(self.path + 'png_folder/losses' + '.png', bbox_inches='tight')
         plt.close(fig)
-      
+
       fig, ax = plt.subplots(5, time_steps , figsize = (15,10))
       for i in range(0, time_steps):
-        
+
         ax[0,i].imshow(self.convert_to_numpy(image[0, i, :, :, :]))
         ax[0,i].set_title("True Image")
         ax[0,i].axis('off')
-        
+
         ax[1,i].imshow(self.convert_to_numpy(samples[i, 0, :, :, :]))
         ax[1,i].set_title("Sample|1 frame")
         ax[1,i].axis('off')
-        
+
         ax[2,i].imshow(self.convert_to_numpy(plot_preds[i, 0, :, :, :]))
         ax[2,i].set_title("Prediction")
         ax[2,i].axis('off')
-        
+
         ax[3,i].imshow(self.convert_to_numpy(recons[i, 0, :, :, :]))
         ax[3,i].set_title("Reconstruction")
         ax[3,i].axis('off')
-        
+
         ax[4,i].imshow(self.convert_to_numpy(recons[i, 0, :, :, :]))
         ax[4,i].set_title("Recon-Bijection")
         ax[4,i].axis('off')
-        
+
       if not self.verbose:
         fig.tight_layout()
         fig.savefig(self.path +'png_folder/samples'+ n_plot + '.png', bbox_inches='tight')
         plt.close(fig)
-        
+
       if self.verbose:
         print("\tKL and Reconstruction loss: {:.4f}, {:.4f}".format(self.kl_loss[-1].data, self.recon_loss[-1].data))
         plt.show()
- 
+
       self.plot_counter += 1
       self.model.train()
 
@@ -400,11 +400,11 @@ class Solver(object):
             ax[0,i].imshow(self.convert_to_numpy(image[0, i, :, :, :]))
             ax[0,i].set_title("True Image")
             ax[0,i].axis('off')
-            for z, zname in zip(range(0,2),list(['Prior','Encoder'])): 
+            for z, zname in zip(range(0,2),list(['Prior','Encoder'])):
                 ax[1+z,i].imshow(self.convert_to_numpy(recons[z, i, 0, :, :, :]))
                 ax[1+z,i].set_title(str(zname))
                 ax[1+z,i].axis('off')
-                
+
                 ax[3+z,i].imshow(self.convert_to_numpy(recons_flow[z,i, 0, :, :, :]))
                 ax[3+z,i].set_title(str(zname)+' flow')
                 ax[3+z,i].axis('off')
@@ -434,7 +434,6 @@ class Solver(object):
         plt.ylabel("Average ll posterior")
         fig.savefig(self.path + 'png_folder/KLDdiagnostic' + '.png', bbox_inches='tight')
         plt.close(fig)
-	
+
     def convert_to_numpy(self, x):
         return x.permute(1,2,0).squeeze().detach().cpu().numpy()
-
--- VRNN/trainer.py
+++ VRNN/trainer.py
@@ -9,7 +9,7 @@ from data_generators import PushDataset
 import matplotlib.pyplot as plt
 from .VRNN import VRNN
 from Utils import set_gpu
-from tqdm import tqdm 
+from tqdm import tqdm
 device = set_gpu(True)
 
 class EarlyStopping():
@@ -28,7 +28,7 @@ class EarlyStopping():
 		current_loss = loss
 		if current_loss is None:
 			pass
-		else: 
+		else:
 			if (current_loss - self.best_loss) < -self.min_delta:
 				self.best_loss = current_loss
 				self.wait = 1
@@ -39,7 +39,7 @@ class EarlyStopping():
 					  print("STOP! Criterion met at epoch " % (self.epoch))
 					return self.stop_training
 				self.wait += 1
- 
+
 class Solver(object):
     def __init__(self, args):
         self.args = args
@@ -74,52 +74,52 @@ class Solver(object):
         self.multigpu = args.multigpu
         self.n_predictions = args.n_predictions
         self.n_conditions = args.n_conditions
-        
+
     def build(self):
         self.train_loader, self.test_loader = self.create_loaders()
-        
+
         if not os.path.exists(self.path + 'png_folder'):
           os.makedirs(self.path + 'png_folder')
         if not os.path.exists(self.path + 'model_folder'):
           os.makedirs(self.path + 'model_folder')
-        
+
         model = VRNN(self.args)
-        
+
         if self.multigpu and torch.cuda.device_count() > 1:
             print("Using:", torch.cuda.device_count(), "GPUs")
             self.model = nn.DataParallel(model).to(device)
         else:
             self.model = model.to(device)
-        
+
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
-        self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, 'min', 
-                                                                    patience=self.patience_lr, 
-                                                                    factor=self.factor_lr, 
+        self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, 'min',
+                                                                    patience=self.patience_lr,
+                                                                    factor=self.factor_lr,
                                                                     min_lr=self.min_lr,
                                                                     verbose = True)
-        self.earlystopping = EarlyStopping(min_delta = 0, patience = self.patience_es, 
+        self.earlystopping = EarlyStopping(min_delta = 0, patience = self.patience_es,
                                            verbose = self.verbose)
         self.counter = 0
-        
+
     def create_loaders(self):
         if self.choose_data=='mnist':
-            	testset = MovingMNIST(False, 'Mnist', 
-                                                         seq_len=self.n_frames, 
-                                                         image_size=self.image_size, 
-                                                         digit_size=self.digit_size, 
-                                                         num_digits=self.num_digits, 
-            												 deterministic=False, 
-                                                         three_channels=False, 
-                                                         step_length=self.step_length, 
+            	testset = MovingMNIST(False, 'Mnist',
+                                                         seq_len=self.n_frames,
+                                                         image_size=self.image_size,
+                                                         digit_size=self.digit_size,
+                                                         num_digits=self.num_digits,
+            												 deterministic=False,
+                                                         three_channels=False,
+                                                         step_length=self.step_length,
                                                          normalize=False)
-            	trainset = MovingMNIST(True, 'Mnist', 
-                                                          seq_len=self.n_frames, 
-                                                          image_size=self.image_size, 
-                                                          digit_size=self.digit_size, 
-                                                          num_digits=self.num_digits, 
-            												  deterministic=False, 
-                                                          three_channels=False, 
-                                                          step_length=self.step_length, 
+            	trainset = MovingMNIST(True, 'Mnist',
+                                                          seq_len=self.n_frames,
+                                                          image_size=self.image_size,
+                                                          digit_size=self.digit_size,
+                                                          num_digits=self.num_digits,
+            												  deterministic=False,
+                                                          three_channels=False,
+                                                          step_length=self.step_length,
                                                           normalize=False)
         if self.choose_data=='bair':
             	string=str(os.path.abspath(os.getcwd()))
@@ -133,7 +133,7 @@ class Solver(object):
         train_loader = DataLoader(trainset,batch_size=self.batch_size, shuffle=True, drop_last=True)
         test_loader = DataLoader(testset, batch_size=self.batch_size, shuffle=True, drop_last=True)
         return train_loader, test_loader
- 
+
     def preprocess(self, x, reverse = False):
         # Remember to change the scale parameter to make variable between 0..255
         preprocess_range = self.preprocess_range
@@ -156,7 +156,7 @@ class Solver(object):
             x = x * scale
             if n_bits < 8:
               x = torch.floor( x/2 ** (8 - n_bits))
-            x = x / n_bins  
+            x = x / n_bins
           else:
             x = torch.clamp(x, 0, 1)
             x = x * n_bins
@@ -169,14 +169,14 @@ class Solver(object):
         else:
             print("Invalid preprocess choice")
         return x
-    
+
     def compute_loss(self, nll, kl, dims, t=1):
         loss = nll + self.beta*kl
-        
+
         kl_store = kl.data
         nll_store = nll.data
         elbo = -(kl_store+nll_store)
-        
+
         #https://stats.stackexchange.com/questions/423120/what-is-bits-per-dimension-bits-dim-exactly-in-pixel-cnn-papers/431012
         bits_per_dim_loss = -elbo/(np.log(2.)*torch.prod(torch.tensor(dims))*t)
         self.bits.append(float(bits_per_dim_loss))
@@ -184,7 +184,7 @@ class Solver(object):
         self.kl_loss.append(float(kl_store)/t)
         self.recon_loss.append(float(nll_store)/t)
         return loss
-    
+
     def train(self):
       max_value = self.beta_max
       min_value = self.beta_min
@@ -201,7 +201,7 @@ class Solver(object):
             else:
                 image = image.to(device)
             image = self.preprocess(image)
-            
+
             self.beta = min(max_value, min_value + self.counter*(max_value - min_value) / num_steps)
 
             if self.multigpu and torch.cuda.device_count() > 1:
@@ -215,27 +215,27 @@ class Solver(object):
             self.optimizer.step()
             self.counter += 1
 
-          self.plotter()   
+          self.plotter()
           epoch_loss = np.mean(self.losses)
- 
+
           if self.epoch_i % 1 == 0:
             # Save model after each 25 epochs
-            self.checkpoint('vrnn.pt', self.epoch_i, epoch_loss) 
- 
+            self.checkpoint('vrnn.pt', self.epoch_i, epoch_loss)
+
           # Do early stopping, if above patience stop training, if better loss, save model
           stop = self.earlystopping.step(self.epoch_i, epoch_loss)
           if stop:
             break
           if (self.earlystopping.best_loss < self.best_loss) and (self.epoch_i > 50):
             self.best_loss = self.earlystopping.best_loss
-            self.checkpoint('vrnn_best_model.pt', self.epoch_i, epoch_loss) 
+            self.checkpoint('vrnn_best_model.pt', self.epoch_i, epoch_loss)
           self.scheduler.step(epoch_loss)
-          
+
           if self.verbose:
             print('Epoch {} Loss: {:.2f}'.format(self.epoch_i, epoch_loss))
           else:
             self.status()
- 
+
     def checkpoint(self, model_name, epoch, loss):
       torch.save({
           'epoch': epoch,
@@ -250,8 +250,8 @@ class Solver(object):
           'annealing_counter': self.counter,
           'args': self.args,
           }, self.path + 'model_folder/' + model_name)
-      
-  
+
+
     def load(self, load_model):
       self.model.load_state_dict(load_model['model_state_dict'])
       self.optimizer.load_state_dict(load_model['optimizer_state_dict'])
@@ -266,7 +266,7 @@ class Solver(object):
       self.best_loss = loss
       self.model.to(device)
       return (self.epoch_i, loss)
-  
+
     def status(self):
       # Only works for python 3.x
       lr = self.optimizer.param_groups[0]['lr']
@@ -274,7 +274,7 @@ class Solver(object):
         print("STATUS:", file=f)
         print("\tKL and Reconstruction loss: {:.4f}, {:.4f}".format(self.kl_loss[-1], self.recon_loss[-1]), file=f)
         print(f'\tEpoch {self.epoch_i}, Beta value {self.beta:.4f}, Learning rate {lr}', file=f)
- 
+
     def plotter(self):
       n_plot = str(self.plot_counter)
       with torch.no_grad():
@@ -283,81 +283,81 @@ class Solver(object):
         time_steps = self.n_frames
 
         image  = self.preprocess(image, reverse=False)
-        
-        
+
+
         x_conditions, predictions = self.model.predict(image,
                                                  n_predictions = self.n_predictions,
                                                  n_conditions = self.n_conditions)
         plot_preds = torch.cat((x_conditions, predictions), 0)
-        
+
         recons = self.model.reconstruct(image)
         samples = self.model.sample(image, n_samples = time_steps)
-        
+
         samples  = self.preprocess(samples, reverse=True)
         recons  = self.preprocess(recons, reverse=True)
         plot_preds  = self.preprocess(plot_preds, reverse=True)
         image  = self.preprocess(image, reverse=True)
-        
- 
+
+
       fig, ax = plt.subplots(1, 4 , figsize = (20,5))
       ax[0].plot(self.bits)
       ax[0].set_title("Bits per dim")
       ax[0].set_xlabel("mini-batch")
       ax[0].set_ylabel("bits-per-dim")
       ax[0].grid()
-      
+
       ax[1].plot(self.losses)
       ax[1].set_title("Log of the loss")
       ax[1].set_yscale('log')
       ax[1].set_xlabel("mini-batch")
       ax[1].set_ylabel("log-loss")
       ax[1].grid()
-      
+
       ax[2].plot(self.kl_loss)
       ax[2].set_title("KL Loss")
       ax[2].grid()
       ax[2].set_xlabel("mini-batch")
       ax[2].set_ylabel("KL")
-      
+
       ax[3].plot(self.recon_loss)
       ax[3].set_title("Reconstruction Loss")
       ax[3].grid()
       ax[3].set_xlabel("mini-batch")
       ax[3].set_ylabel("nll")
-      
+
       if not self.verbose:
-	fig.tight_layout()
+		fig.tight_layout()
         fig.savefig(self.path + 'png_folder/losses' + '.png', bbox_inches='tight')
         plt.close(fig)
-      
+
       fig, ax = plt.subplots(4, time_steps , figsize = (15,10))
       for i in range(0, time_steps):
         ax[0,i].imshow(self.convert_to_numpy(image[0, i, :, :, :]))
         ax[0,i].set_title("True Image")
         ax[0,i].axis('off')
-        
+
         ax[1,i].imshow(self.convert_to_numpy(samples[i, 0, :, :, :]))
         ax[1,i].set_title("Sample|1 frame")
         ax[1,i].axis('off')
-        
+
         ax[2,i].imshow(self.convert_to_numpy(plot_preds[i, 0, :, :, :]))
         ax[2,i].set_title("Prediction")
         ax[2,i].axis('off')
-        
+
         ax[3,i].imshow(self.convert_to_numpy(recons[i, 0, :, :, :]))
         ax[3,i].set_title("Reconstruction")
         ax[3,i].axis('off')
-        
+
 
       if not self.verbose:
-	fig.tight_layout()
+		fig.tight_layout()
         fig.savefig(self.path +'png_folder/samples' + n_plot + '.png', bbox_inches='tight')
         plt.close(fig)
 
       if self.verbose:
         print("\tKL and Reconstruction loss: {:.4f}, {:.4f}".format(self.kl_loss[-1].data, self.recon_loss[-1].data))
         plt.show()
- 
+
       self.plot_counter += 1
       self.model.train()
 
