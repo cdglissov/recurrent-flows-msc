2e183e01985e4ea63ae9231e58300e832971c679
TobiasKonradsen <43344305+TobiasKonradsen@users.noreply.github.com>
2021-01-22 16:57:40 +0100

    Update Plot_Smarter.py

--- evaluation_metrics/Plot_Smarter.py
+++ evaluation_metrics/Plot_Smarter.py
@@ -4,11 +4,16 @@ import numpy as np
 # For the loss now
 from scipy.signal import savgol_filter
 
-namelist = ['tanh_no','tanhup_down','tanh_no_conv','tanhup_down_conv']
-path = './tanhtest/'
+namelist = ['overshot0_v3','overshot1_v3','overshot2_v3']
+labelnames = ['D=1','D=2','D=3']
+path = './SRNNtest/'
 fig, ax = plt.subplots(3, 2 ,figsize = (20,10)) #, figsize = (time_steps,5*num_samples))
+markersize = 5
+n_conditions = 5
+n_train = 10
 for i in range(0,len(namelist)):
     name = namelist[i]
+    lname = labelnames[i]
     plot_dict = torch.load(path+name+'/PSNR_SSIM_LPIPS.pt')
     SSIM  = plot_dict['SSIM_values_sklearn']
     PSNR  = plot_dict['PSNR_values_sklearn']
@@ -16,46 +21,66 @@ for i in range(0,len(namelist)):
     y = SSIM.mean(0).numpy()
     twostd = 1.96 * np.std(SSIM.numpy(),0)#/np.sqrt(np.shape(SSIM.numpy())[0])
     #print(twostd)
-    ax[0,0].plot(np.arange(0,len(y)),y,label = name)
+    xaxis = np.arange(0+n_conditions,len(y)+n_conditions)
+    ax[0,0].plot(xaxis,y,label = lname, marker='o', markersize=markersize)
     alpha = 0.05
-    ax[0,0].fill_between(np.arange(0,len(y)), y-twostd, y+twostd, alpha=.1)
+    ax[0,0].fill_between(xaxis, y-twostd, y+twostd, alpha=.1)
     y = np.median(SSIM.numpy(),0)
-    ax[0,1].plot(np.arange(0,len(y)),y,label = name)
+    ax[0,1].plot(xaxis,y,label = lname, marker='o',markersize=markersize)
     alpha = 0.05
-    ax[0,1].fill_between(np.arange(0,len(y)), np.quantile(SSIM.numpy(),alpha/2,axis = 0), np.quantile(SSIM.numpy(),1-alpha/2,axis = 0), alpha=.1)
+    ax[0,1].fill_between(xaxis, np.quantile(SSIM.numpy(),alpha/2,axis = 0), np.quantile(SSIM.numpy(),1-alpha/2,axis = 0), alpha=.1)
     y = PSNR.mean(0).numpy()
-    twostd = 1.96 * np.std(PSNR.numpy(),0)
-    ax[1,0].plot(np.arange(0,len(y)),y,label = name)
-    ax[1,0].fill_between(np.arange(0,len(y)), y-twostd, y+twostd, alpha=.1)
+    twostd = 1.96 * np.std(PSNR.numpy(),0)#/np.sqrt(np.shape(PSNR.numpy())[0])
+    ax[1,0].plot(xaxis,y,label = lname, marker='o', markersize=markersize)
+    ax[1,0].fill_between(xaxis, y-twostd, y+twostd, alpha=.1)
     y = np.median(PSNR.numpy(),0)
-    ax[1,1].plot(np.arange(0,len(y)),y,label = name)
-    ax[1,1].fill_between(np.arange(0,len(y)), np.quantile(PSNR.numpy(),alpha/2,axis = 0), np.quantile(PSNR.numpy(),1-alpha/2,axis = 0), alpha=.1)
+    ax[1,1].plot(xaxis,y,label = lname, marker='o',markersize=markersize)
+    ax[1,1].fill_between(xaxis, np.quantile(PSNR.numpy(),alpha/2,axis = 0), np.quantile(PSNR.numpy(),1-alpha/2,axis = 0), alpha=.1)
     y = LPIPS.mean(0).numpy()
-    twostd = 1.96 * np.std(LPIPS.numpy(),0)
-    ax[2,0].plot(np.arange(0,len(y)),y,label = name)
+    twostd = 1.96 * np.std(LPIPS.numpy(),0)#/np.sqrt(np.shape(LPIPS.numpy())[0])
+    ax[2,0].plot(xaxis,y,label = lname, marker='o',markersize=markersize)
     alpha = 0.05
-    ax[2,0].fill_between(np.arange(0,len(y)), y-twostd, y+twostd, alpha=.1)
+    ax[2,0].fill_between(xaxis, y-twostd, y+twostd, alpha=.1)
     y = np.median(LPIPS.numpy(),0)
-    ax[2,1].plot(np.arange(0,len(y)),y,label = name)
+
+    ax[2,1].plot(np.arange(0,len(y)),y,label = lname, marker='o',markersize=markersize)
     alpha = 0.05
     ax[2,1].fill_between(np.arange(0,len(y)), np.quantile(LPIPS.numpy(),alpha/2,axis = 0), np.quantile(LPIPS.numpy(),1-alpha/2,axis = 0), alpha=.1)
     
-ax[0,0].set_title('SSIM')
+ax[0,0].set_ylabel('SSIM')
+ax[0,0].set_xlabel('t')
+ax[0,0].axvline(x=n_train, color='k', linestyle='--')
 ax[0,0].legend()
 ax[0,0].grid()
-ax[0,1].set_title('SSIM-quantile-plot')
+
+ax[0,1].set_ylabel('SSIM-quantile-plot')
+ax[0,1].set_xlabel('t')
+ax[0,1].axvline(x=n_train, color='k', linestyle='--')
 ax[0,1].legend()
 ax[0,1].grid()
-ax[1,0].set_title('PSNR')
+
+ax[1,0].set_ylabel('PSNR')
+ax[1,0].set_xlabel('t')
+ax[1,0].axvline(x=n_train, color='k', linestyle='--')
 ax[1,0].legend()
 ax[1,0].grid()
-ax[1,1].set_title('PSNR-quantile-plot')
+
+
+ax[1,1].set_ylabel('PSNR-quantile-plot')
+ax[1,1].set_xlabel('t')
+ax[1,1].axvline(x=n_train, color='k', linestyle='--')
 ax[1,1].legend()
 ax[1,1].grid()
-ax[2,0].set_title('LPIPS')
+
+ax[2,0].set_ylabel('LPIPS')
+ax[2,0].set_xlabel('t')
+ax[2,0].axvline(x=n_train, color='k', linestyle='--')
 ax[2,0].legend()
 ax[2,0].grid()
-ax[2,1].set_title('LPIPS-quantile-plot')
+
+ax[2,1].set_ylabel('LPIPS-quantile-plot')
+ax[2,1].set_xlabel('t')
+ax[2,1].axvline(x=n_train, color='k', linestyle='--')
 ax[2,1].legend()
 ax[2,1].grid()
 fig.savefig(path +  'SSIM_PSNR_LPIPS.png', bbox_inches='tight')  
@@ -67,7 +92,7 @@ fig2, ax2 = plt.subplots(3, 1)
 for i in range(0,len(namelist)):
 	name = namelist[i]
 	pathcd = path #'/work1/s144077/tanhtest/'
-	pathmodel = pathcd+namelist[i]+'/model_folder/eval_dict.pt'
+	pathmodel = pathcd+namelist[i]+'/model_folder/srnn.pt'
     
 	load_dict = torch.load(pathmodel)
 	loss = load_dict['losses']
